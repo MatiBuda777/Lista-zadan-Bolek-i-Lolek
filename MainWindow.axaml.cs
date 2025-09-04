80cddeb5a610978e5b409077647a5569537b007f
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2025-09-04 11:28:54 +0200

    First commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ListaZadanBolkaILolka;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
