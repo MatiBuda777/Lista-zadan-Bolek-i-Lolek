7ab9b57d25f7e5a704887df914b2721dfd5e108b
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2025-09-11 11:25:21 +0200

    Polished all existing functions

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace ListaZadanBolkaILolka;
 
@@ -17,24 +21,74 @@ public partial class MainWindow : Window
 
     private void AddTaskButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        string selectedCharacter = CharacterSelectComboBox?.SelectionBoxItem?.ToString() ?? "";
-        string taskDescription = TaskNameTextBox?.Text ?? "";
+        string selectedCharacter = CharacterSelectComboBox?.SelectionBoxItem?.ToString() ?? "Nie wybrano postaci";
+        string taskDescription = TaskNameTextBox?.Text ?? "Brak opisu";
         string priority = LowPriorityRadioButton.IsChecked == true ? "Niski priorytet" :
             MidPriorityRadioButton.IsChecked == true ? "Normalny priorytet" :
             HighPriorityRadioButton.IsChecked == true ? "Wysoki priorytet" :
-            "";
-        
-        var text = $"{selectedCharacter} - {taskDescription} [{priority}]"; // add error message for empty fields?
+            "Bez priorytetu";
+
+        var infoList = new List<string>();
+
+        if (OutdoorsTask.IsChecked == true) infoList.Add("Zadanie na dworzu");
+        if (RequiredToolsTask.IsChecked == true) infoList.Add("Zadanie wymagające sprzętu");
+        if (TeamTask.IsChecked == true) infoList.Add("Zadanie drużynowe");
+
+        var additionalInfo = string.Join(", ", infoList);
+
+        var text = $"{selectedCharacter} - {taskDescription} [{priority}]. Dodatkowe Info: {additionalInfo}"; // add error message for empty fields?
         _taskItems.Add(text);
     }
 
     private void DeleteTasksButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            var itemsToRemove = TaskListBox?.SelectedItems?.Cast<string>().ToList();
+            foreach (var item in itemsToRemove)
+            {
+                _taskItems.Remove(item);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        
     }
 
     private void CharacterSelectComboBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        var selectedCharacter = CharacterSelectComboBox?.SelectionBoxItem?.ToString();
+        CharacterShowTextBlock.Text = selectedCharacter;
+        switch (selectedCharacter)
+        {
+            case "Bolek":
+            {
+                try
+                {
+                    using var stream = AssetLoader.Open(new Uri("avares://ListaZadanBolkaILolka/Images/Bolek.jpg"));
+                    CharacterShowImage.Source = new Bitmap(stream);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                break;
+            }
+            case "Lolek":
+            {
+                try
+                {
+                    using var stream = AssetLoader.Open(new Uri("avares://ListaZadanBolkaILolka/Images/Lolek.jpg"));
+                    CharacterShowImage.Source = new Bitmap(stream);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                break;
+            }
+        }
     }
 }
